bb28adb2e195776b3ed094dd7f2f1c25221cbcbf
Shivam Mishra <shivamalpha@localhost.localdomain>
2020-07-14 02:19:38 +0530

    v1

---
+++ app.js
@@ -0,0 +1,13 @@
+const heading = 'PLEASE MEET OUR TEAM';
+let i = 0;
+console.log(heading.length);
+const typing= () => {
+	if(i < heading.length){
+		document.querySelector('.heading').innerHTML += heading.charAt(i);
+		i++;
+
+		setTimeout(typing, 150);
+	}
+}
+
+typing();
